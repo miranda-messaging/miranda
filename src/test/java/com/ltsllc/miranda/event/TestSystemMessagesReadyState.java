36bc933c217967852f11c66971ff1271739d9598
Clark Hobbie <clark.hobbie@gmail.com>
2017-02-22 19:58:01 -0700

    Removed the test for SystemMessagesReadyState (it just subclasses DirectoryReadyState)

--- src/test/java/com/ltsllc/miranda/event/TestSystemMessagesReadyState.java
+++
@@ -1,9 +0,0 @@
-package com.ltsllc.miranda.event;
-
-import com.ltsllc.miranda.TestCase;
-
-/**
- * Created by Clark on 2/22/2017.
- */
-public class TestSystemMessagesReadyState extends TestCase {
-}
