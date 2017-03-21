c5668a617f3a28ba5c68ffd79a1eeb391bedf6f3
Clark Hobbie <clark.hobbie@gmail.com>
2017-03-20 18:20:02 -0600

    Added TestNetworkReadyState

---
+++ src/test/java/com/ltsllc/miranda/network/TestNetworkReadyState.java
@@ -0,0 +1,120 @@
+package com.ltsllc.miranda.network;
+
+import com.ltsllc.miranda.Message;
+import com.ltsllc.miranda.network.messages.ConnectToMessage;
+import com.ltsllc.miranda.network.messages.SendNetworkMessage;
+import com.ltsllc.miranda.node.networkMessages.JoinWireMessage;
+import com.ltsllc.miranda.test.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Matchers;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by Clark on 3/20/2017.
+ */
+public class TestNetworkReadyState extends TestCase {
+    private NetworkReadyState networkReadyState;
+
+    public NetworkReadyState getNetworkReadyState() {
+        return networkReadyState;
+    }
+
+    public void reset () {
+        super.reset();
+
+        networkReadyState = null;
+    }
+
+    @Before
+    public void setup () {
+        reset();
+
+        super.setup();
+
+        setuplog4j();
+
+        networkReadyState = new NetworkReadyState(getMockNetwork());
+    }
+
+    @Test
+    public void testProcessConnectToMessage () {
+        ConnectToMessage connectToMessage = new ConnectToMessage("foo.com", 6789, null, this);
+
+        getNetworkReadyState().processMessage(connectToMessage);
+
+        verify(getMockNetwork(), atLeastOnce()).connect(Matchers.any(ConnectToMessage.class));
+    }
+
+    @Test
+    public void testProcessSendNetworkMessageSuccess () {
+        JoinWireMessage joinWireMessage = new JoinWireMessage("foo.com", "192.168.1.1", 6789, "a node");
+        SendNetworkMessage sendNetworkMessage = new SendNetworkMessage(null, this, joinWireMessage, -1);
+
+        getNetworkReadyState().processMessage(sendNetworkMessage);
+
+        try {
+            verify(getMockNetwork(), atLeastOnce()).sendOnNetwork(Matchers.any(SendNetworkMessage.class));
+        } catch (NetworkException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    @Test
+    public void testProcessSendNetworkMessageExceptionUnrecognizedHandle () {
+        BlockingQueue<Message> queue = new LinkedBlockingQueue<Message>();
+        JoinWireMessage joinWireMessage = new JoinWireMessage("foo.com", "192.168.1.1", 6789, "a node");
+        SendNetworkMessage sendNetworkMessage = new SendNetworkMessage(queue, this, joinWireMessage, -1);
+        NetworkException networkException = new NetworkException("Test", NetworkException.Errors.UnrecognizedHandle);
+
+        try {
+            doThrow(networkException).when(getMockNetwork()).sendOnNetwork(Matchers.any(SendNetworkMessage.class));
+        } catch (NetworkException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        getNetworkReadyState().processMessage(sendNetworkMessage);
+
+        try {
+            verify(getMockNetwork(), atLeastOnce()).sendOnNetwork(Matchers.any(SendNetworkMessage.class));
+        } catch (NetworkException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        assert (contains(Message.Subjects.UnknownHandle, queue));
+    }
+
+    @Test
+    public void testProcessSendNetworkMessageException () {
+        BlockingQueue<Message> queue = new LinkedBlockingQueue<Message>();
+        JoinWireMessage joinWireMessage = new JoinWireMessage("foo.com", "192.168.1.1", 6789, "a node");
+        SendNetworkMessage sendNetworkMessage = new SendNetworkMessage(queue, this, joinWireMessage, -1);
+        NetworkException networkException = new NetworkException("Test", NetworkException.Errors.ExceptionSending);
+
+        try {
+            doThrow(networkException).when(getMockNetwork()).sendOnNetwork(Matchers.any(SendNetworkMessage.class));
+        } catch (NetworkException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        getNetworkReadyState().processMessage(sendNetworkMessage);
+
+        try {
+            verify(getMockNetwork(), atLeastOnce()).sendOnNetwork(Matchers.any(SendNetworkMessage.class));
+        } catch (NetworkException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        assert (contains(Message.Subjects.NetworkError, queue));
+
+    }
+}
