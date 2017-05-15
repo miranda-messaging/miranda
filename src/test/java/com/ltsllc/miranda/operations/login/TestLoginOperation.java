63fedc325a3032da98977536a67c6ed6538e0955
Clark Hobbie <clark.hobbie@gmail.com>
2017-05-15 05:47:06 -0600

    Added TestLoginOperation

--- src/test/java/com/ltsllc/miranda/operations/login/TestLoginOperation.java
+++ src/test/java/com/ltsllc/miranda/operations/login/TestLoginOperation.java
@@ -4,8 +4,14 @@ import com.ltsllc.miranda.Message;
 import com.ltsllc.miranda.test.TestCase;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Matchers;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Clark on 5/15/2017.
@@ -29,6 +35,8 @@ public class TestLoginOperation extends TestCase {
 
         super.setup();
 
+        setuplog4j();
+
         loginOperation = new LoginOperation("whatever", new LinkedBlockingQueue<Message>());
     }
 
@@ -36,5 +44,16 @@ public class TestLoginOperation extends TestCase {
     public void testConstructor () {
         assert (getLoginOperation().getUser().equals("whatever"));
         assert (getLoginOperation().getCurrentState() instanceof LoginOperationReadyState);
+    }
+
+    @Test
+    public void testStart () {
+        setupMockMiranda();
+        when(getMockMiranda().getSessionManager()).thenReturn(getMockSessionManager());
+
+        getLoginOperation().start();
+
+        verify(getMockSessionManager(), atLeastOnce()).sendGetSessionMessage(Matchers.any(BlockingQueue.class),
+                Matchers.any(), Matchers.anyString());
     }
 }
